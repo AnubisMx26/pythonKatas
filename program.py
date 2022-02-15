b9cde589d9339607fbef45bd81c816f8f8d8cd1a
AnubisMx26 <33039114+AnubisMx26@users.noreply.github.com>
2022-02-14 21:05:20 -0600

    Add files via upload

---
+++ program.py
@@ -0,0 +1,14 @@
+ # program.py
+sum = 1 + 2
+print(sum)
+
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
